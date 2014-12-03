303478a6af359f6b1b1199ff6e74c641a96f94b1
na57 <na57@qq.com>
2014-12-03 17:06:41 +0800

    yo
    使用yo创建项目

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zcProjectSbApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zcProjectSbApp
+ */
+angular.module('zcProjectSbApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zcProjectSbApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zcProjectSbApp
+ */
+angular.module('zcProjectSbApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
